183c3d6a5a82294fae11b87e52a8636c49cc8d79
Muhammad Soban <fa18-bcs-058@cuilahore.edu.pk>
2022-01-20 12:41:51 +0500

    pagination and sorting

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { query, Request, Response } from "express";
 import Todos from "../models/todo.model";
 
 export const getAllTodos = async (
@@ -6,8 +6,19 @@ export const getAllTodos = async (
   res: Response
 ): Promise<any> => {
   try {
-    const todos = await Todos.find();
-    return res.status(200).json({ results: todos.length, todos });
+    const { page = 1, limit = 5, sort = "todo" } = req?.query;
+    let todos = await Todos.find()
+      .limit(+limit * 1)
+      .skip((+page - 1) * +limit)
+      .sort(sort)
+      .exec();
+
+    return res.status(200).json({
+      count: todos.length,
+      page,
+      totalPages: Math.ceil((await Todos.countDocuments()) / +limit),
+      todos,
+    });
   } catch (error) {
     return res.status(404).json({ message: error.message });
   }
